a2eb2c7608cb17443f42921f4bc97070ae3a38fe
Daniel Schreyer <daniel.schreyer.ext@boehringer-ingelheim.com>
2024-09-30 15:12:19 +0200

    improved readability of dsoParams using yaml format (#12)
    * improved readability using as.yaml and cat
    
    * removed redundant functions

--- R/class-dsoParams.R
+++ R/class-dsoParams.R
@@ -1,25 +1,25 @@
 #' A "dsoParams" and its constructor:
-#' 
-#' @title dsoParams: list with safe access 
+#'
+#' @title dsoParams: list with safe access
 #' @param x empty, or a recursive list of lists which is converted to dsoParams
 #' @examples
 #' # initiating empty
 #' params <- dsoParams()
-#' 
-#' # converting a list of list 
-#' 
+#'
+#' # converting a list of list
+#'
 #' params <- list()
 #' params$a <- "bla"
 #' params$b <- list()
 #' params$b$c <- "blub"
 #' params <- dsoParams(params)
-#' 
+#'
 #' @export
 dsoParams <- function( x = list()) {
   if(! is.list(x))
     stop("x needs to be a list or a list of lists.")
-  
-  # recursively 
+
+  # recursively
   x <- lapply(x, function(y) {
       if(is.list(y)) {
         dsoParams(y)
@@ -39,7 +39,7 @@ dsoParams <- function( x = list()) {
   if (!name %in% names(x)) {
     stop(paste("Element '", name, "' does not exist in dsoParams", sep = ""))
   }
-  
+
   NextMethod()
 }
 
@@ -47,14 +47,24 @@ dsoParams <- function( x = list()) {
 #' it cannot return NULL when call does not exist
 #' @export
 `[[.dsoParams` <- function(x, i, ...) {
-  
   if (is.character(i) && !i %in% names(x)) {
     stop(paste("Element '", i, "' does not exist in dsoParams", sep = ""))
   } else if (is.numeric(i) && (i < 1 || i > length(x))) {
     stop(paste("Index '", i, "' is out of bounds in dsoParams", sep = ""))
   }
-  
   NextMethod()
 }
 
+#' Custom print method for dsoParams class
+#' @export
+print.dsoParams <- function(object, ...) {
+  cat(yaml::as.yaml(object))
+}
 
+#' Custom show method for dsoParams class
+#' @export
+setMethod(f = "show",
+          signature = "dsoParams",
+          definition = function(object) {
+            cat(yaml::as.yaml(object))
+          })
